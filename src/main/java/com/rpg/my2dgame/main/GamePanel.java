aff06926425249ecb4c5e39df06e2823c7e4a836
Madhav Tyagi <realmadhavtyagi@gmail.com>
2024-02-21 00:16:26 +0530

    Update GamePanel.java

--- src/main/java/com/rpg/my2dgame/main/GamePanel.java
+++ src/main/java/com/rpg/my2dgame/main/GamePanel.java
@@ -7,23 +7,22 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel {
     
     // Screen Settings
-    private final int originalTileSize = 16;
-    private final int scale = 3;
+    private final int ORIGINAL_TILE_SIZE = 16;
+    private final int SCALE = 3;
     
-    private final int tileSize = originalTileSize * scale;
-    private final int maxScreenCol = 16;
-    private final int maxScreenRow = 12;
-    private final int screenWidth = tileSize * maxScreenCol;
-    private final int screenHeight = tileSize * maxScreenRow;
+    private final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    private final int MAX_SCREEN_COL = 16;
+    private final int MAX_SCREEN_ROW = 12;
+    private final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;
+    private final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
     
     public GamePanel() {
         initializePanel();
     }
 
     private void initializePanel() {
-        setPreferredSize(new Dimension(screenWidth, screenHeight));
+        setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setBackground(Color.BLACK);
         setDoubleBuffered(true);
     }
 }
-
